003dadc978b8ec0bfe2fed1a6efa618127afa98e
Sruthika024 <ksruthika024@gmail.com>
2025-05-16 20:08:00 +0530

    Delete DLFS-main directory

--- DLFS-main/run_scripts/train.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --gpu_ids 0 --dataroot ./datasets/males --name males_model --batchSize 2 --verbose
--- DLFS-main/test_distan.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python test_distan.py --verbose --dataroot ./datasets/males --name males_model --which_epoch 360 --how_many 100 --display_id 0 --encoder_type distan --checkpoints_dir mst_1_cpt
